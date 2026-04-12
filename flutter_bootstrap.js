c750a2c3aec3a409bb95e521c79f1f97f9be4268
SahalCk <79449914+SahalCk@users.noreply.github.com>
2026-04-12 21:16:56 +0530

    Add files via upload
    fixed some bugs

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,7 +39,7 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1460460351",
+    serviceWorkerVersion: "2893548264",
   },
   onEntrypointLoaded: async function(engineInitializer) {
     let appRunner = await engineInitializer.initializeEngine({
